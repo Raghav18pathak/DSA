d03d2437b2ef27d9e57e90dbcff084045e46f86f
Raghav18pathak <raghavpathak7452@gmail.com>
2026-04-29 09:27:35 +0530

    Update code.cpp

--- LONGEST SUBARRAY POSITIVE/code.cpp
+++ LONGEST SUBARRAY POSITIVE/code.cpp
@@ -1,17 +1,17 @@
 int longestSubarrayWithSumK(vector<int> a, long long k) {
     // Write your code here
-    int length = 0;
+    int result = 0;
+    long long sum=0;
+    int j=0;
     for(int i=0;i<a.size();i++){
-        long long sum=0;
-        int count =0;
-        for(int j=i;j<a.size();j++){
-            sum = sum+a[j];
-            count ++;
-            if(sum == k && count > length){
-                length = count;
-            }
-            if(sum>k) break;
+        sum = sum + a[i];
+        if(sum == k && i-j+1 > result) result = i-j+1;
+        if(sum > k){
+            sum = sum - a[j];
+            sum = sum - a[i];
+            j++;
+            i--;
         }
     }
-    return length;
+    return result;
 }
