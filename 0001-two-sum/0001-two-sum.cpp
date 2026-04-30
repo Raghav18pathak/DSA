5ed2b19536dcbc9e93621b0e12318252c7d0d223
Raghav18pathak <raghavpathak7452@gmail.com>
2026-04-30 13:51:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_multimap<int,int> mpp;
         for(int i=0;i<nums.size();i++){
-            for(int j = i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
-                    return {i,j};
+            int rem = target-arr[i];
+                if(mpp.find(rem)!=mpp.end()){
+                    return {mpp[rem],i}
                 }
+                mpp[sum]=i;
             }
         }
         return {};
