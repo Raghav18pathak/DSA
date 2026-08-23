bcdbaa503d201f2f00043864c0adda46eb2da435
Raghav18pathak <raghavpathak7452@gmail.com>
2026-08-23 22:20:04 +0530

    Time: 12 ms (17.67%), Space: 13.4 MB (38.13%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,46 +1,15 @@
 class Solution {
 public:
     int romanToInt(string s) {
+        unordered_map<char,int> mpp = {
+            {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
+        };
         int ans = 0;
-        for(int i = 0 ; i < s.size() ; i++){
-            if(s[i] == 'I'){
-                if(i < s.size()-1 && s[i+1] == 'V') {
-                    ans += 4;
-                    i++;
-                }
-                else if(i < s.size()-1 && s[i+1] == 'X') {
-                    ans += 9;
-                    i++;
-                }
-                else ans++;
-            }
-            else if(s[i] == 'X'){
-                if(i < s.size()-1 && s[i+1] == 'L') {
-                    ans += 40;
-                    i++;
-                }
-                else if(i < s.size()-1 && s[i+1] == 'C') {
-                    ans += 90;
-                    i++;
-                }
-                else ans += 10;
-            }
-            else if(s[i] == 'C'){
-                if(i < s.size()-1 && s[i+1] == 'D') {
-                    ans += 400;
-                    i++;
-                }
-                else if(i < s.size()-1 && s[i+1] == 'M') {
-                    ans += 900;
-                    i++;
-                }
-                else ans += 100;
-            }
-            else if (s[i] == 'V') ans += 5;
-            else if (s[i] == 'L') ans += 50;
-            else if (s[i] == 'D') ans += 500;
-            else if (s[i] == 'M') ans += 1000;
+        for(int i = 0 ; i < s.size()-1 ; i++){
+            if(mpp[s[i]] < mpp[s[i+1]]) ans -= mpp[s[i]];
+            else ans += mpp[s[i]];
         }
+        ans+=mpp[s[s.size()-1]];
         return ans;
     }
 };
